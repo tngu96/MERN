597e55ed03b99ed6f168b4f4f3546c6206b166a2
tea <tngu96@outlook.com>
2020-03-18 16:53:54 -0500

    Section 2 Finished

---
+++ config/db.js
@@ -0,0 +1,19 @@
+//the good ol connector using default.json to grab URI of MongoDB/User/Pass
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
